d5ba0f52b33643661a06385aaf8164d659d84132
Jack C <jack@chaker.net>
2021-08-19 01:06:34 -0400

    add stats to homepage

--- bot/src/routes/home.ts
+++ bot/src/routes/home.ts
@@ -4,6 +4,10 @@ const { supportInvite } = require("../../config.json");
 
 import { manager, user } from "../index";
 
+function numberWithCommas(x: number) {
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 const router = express.Router();
 router.get("/", async function (req, res, next) {
 	try {
@@ -13,11 +17,16 @@ router.get("/", async function (req, res, next) {
 			).reduce((acc: any, guildCount) => acc + guildCount, 0),
 			totalUsers: (
 				await manager.fetchClientValues("users.cache.size")
-			).reduce((acc: any, guildCount) => acc + guildCount, 0),
+			).reduce((acc: any, memberCount) => acc + memberCount, 0),
+			totalChannels: (
+				await manager.fetchClientValues("channels.cache.size")
+			).reduce((acc: any, channelCount) => acc + channelCount, 0),
+			totalShards: manager.shards.size,
 			avatar: user.avatarURL,
 			username: user.username,
 			support: supportInvite,
 			invite: `https://discord.com/oauth2/authorize?client_id=${user.id}&permissions=8&scope=bot`,
+			formatNumbers: numberWithCommas,
 		});
 	} catch (error) {
 		next(error);
